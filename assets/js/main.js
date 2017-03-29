23c65289f145cfbc1fae7e6c80e4c9baec6d113e
Nallely House <nallelyht@gmail.com>
2017-03-29 10:51:22 -0600

    agrega elementos a las listas

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,17 @@ function agregarPendiente(){
     var elemento =document.createElement("dd");
     elemento.innerText = nPendiente;
     document.getElementById("listaSemana").appendChild(elemento);
-    
-    console.log(nPendiente)
-    
+    nPendiente="";
+}
+function agregarOtro(){
+    var nPendiente = document.getElementById("otrosPendientes").value;
+    var elemento =document.createElement("dd");
+    elemento.innerText = nPendiente;
+    document.getElementById("listaOtra").appendChild(elemento);
+    nPendiente="";
+} 
+function limpiar (){
+   document.getElementById("nuevoPendiente").value = "";
+   document.getElementById("otrosPendientes").value = "";
     
 }
